08b6bc5b6a7cb2e65be11e1e2033261b01bf3428
Álvaro López García <alvarolopez769@gmail.com>
2022-05-30 12:06:16 +0200

    Externalizado el codigo de manejo de ficheros midi y wav a clases en el directorio utils

---
+++ utils/wav_handler.py
@@ -0,0 +1,31 @@
+import librosa
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Wav_handler:
+
+    def __init__(self, sampling_rate, dir_path):
+        self.sampling_rate = sampling_rate
+        self.dir_path = dir_path
+
+    '''
+        Plotea los valores de la onda del fichero de audio que han pasado como argumento
+        de entrada.
+    '''
+    def plot_wav(self, waves, title=None):
+        wave = waves.flatten()
+        plt.figure(figsize=(16, 6), dpi=200)
+        plt.plot(wave)
+        plt.xlim(0, len(wave))
+        plt.ylim(min(wave) * 1.05, max(wave) * 1.05)
+        if title is not None: plt.title(title)
+        plt.show()
+
+    '''
+        Método que se apoya en la libería librosa para leer un fichero .wav pasado como
+        argumento de entrada y devolver los datos correspondientes a los niveles de onda
+        como un np.array.
+    '''
+    def vectorize_wav(self, file):
+        data, sr = librosa.load(self.dir_path + file, sr=self.sampling_rate)
+        return np.array(data)
